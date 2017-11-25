c80231f3956b6c54fd57ec579ee4ef8b1cbd4e34
Nicolas Peña Escarpentier <npescarpentier@gmail.com>
2017-11-24 20:37:13 -0500

    scene 1 first draft

--- window.js
+++ window.js
@@ -1,7 +1,7 @@
 // global threejs/VR variables
 let container;
 let renderer;
-let curr_scene;
+let current;
 let scenes = [];
 let camera;
 let controls;
@@ -12,6 +12,9 @@ let effect;
 let vrDisplay;    // variable for the display
 let vrButton;     // button to render in VR
 
+// environment variables
+let size = 30;
+
 window.addEventListener('load', onLoad);
 
 function onLoad(){
@@ -20,7 +23,7 @@ function onLoad(){
   let hei = window.innerHeight;
 
   // INITIALIZATION
-  renderer = new THREE.WebGLRenderer({});
+  renderer = new THREE.WebGLRenderer({ });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(wid, hei);
   container.appendChild(renderer.domElement);
@@ -32,7 +35,7 @@ function onLoad(){
 	scenes[3]  = new THREE.Scene();
 	scenes[4]  = new THREE.Scene();
 	scenes[5]  = new THREE.Scene();
-	curr_scene = scenes[1];
+	current = 1;
 
   camera = new THREE.PerspectiveCamera(50, wid/hei, 0.1, 1000);
 	controls = new THREE.VRControls( camera );
@@ -61,7 +64,7 @@ function setupVRStage(){
       vrDisplay = displays[0];
       // setup button
       vrButton = WEBVR.getButton( vrDisplay, renderer.domElement );
-      // document.getElementById('vr_button').appendChild( vrButton );
+      document.getElementById('vr_button').appendChild( vrButton );
     } else {
       console.log("NO VR DISPLAYS PRESENT");
     }
@@ -78,11 +81,11 @@ function animate(timestamp) {
 
   if(vrDisplay.isPresenting){
     controls.update();
-    effect.render(curr_scene, camera);
+    effect.render(scenes[current], camera);
     vrDisplay.requestAnimationFrame(animate);
   } else {
 		controls.update();
-    renderer.render(curr_scene, camera);
+    renderer.render(scenes[current], camera);
     window.requestAnimationFrame(animate);
   }
 }
@@ -107,18 +110,20 @@ function onWindowResize(){
  */
 // create graphic objects
 function createEnvironment(){
-	createScene(0, 0xff0000);
-	createScene(1, 0x00ff00);
-	createScene(2, 0x0000ff);
-}
-function createScene(ind, col){
-  scenes[ind].background = new THREE.Color( 0xeeeeee );
-  let loc = "media/" + ind + "main.gif";
+  let col = [];
+  col[0] = 0xff0000;
+  col[1] = 0x00ff00;
+  col[2] = 0x0000ff;
 
-  createLights(ind);
-  createFloor(ind);
-  createRoom(ind);
-  createSphere(ind, col);
+  for (let i = 0; i < 3; i++) {
+    scenes[i].background = new THREE.Color( 0x555555 );
+    // scenes[i].background = new THREE.Color( 0xeeeeee );
+    createLights(i);
+    createFloor(i);
+    createRoom(i);
+    createSphere(i, col[i]);
+  }
+  scene1();
 }
 function createLights(ind){
   let p_light = new THREE.PointLight(0xffffff, 1, 1000, 2);
@@ -130,8 +135,7 @@ function createLights(ind){
   scenes[ind].add( d_light );
 }
 function createFloor(ind){
-  let size = 40;
-  let floorGeo = new THREE.BoxGeometry(size*2, 1, size*2, 10, 2, 10);
+  let floorGeo = new THREE.BoxGeometry(size*4, 1, size*4, 10, 2, 10);
   let floorMat = new THREE.MeshPhongMaterial({
     color: 0xd0d0d0,
     specular: 0x000000,
@@ -139,33 +143,32 @@ function createFloor(ind){
     wireframe: true,
   });
   let planeF = new THREE.Mesh(floorGeo, floorMat);
-  planeF.position.set(0, -size/5, 0);
+  planeF.position.set(0, -size/4, 0);
   scenes[ind].add(planeF);
 }
 function createRoom(ind){
-  let size = 40;
   let plGeo = new THREE.PlaneGeometry(size, size, 10, 10);
   // images
   let windowMat = new THREE.MeshBasicMaterial({
     map: loader.load("media/" + ind + "/window.png"),
     side: THREE.DoubleSide,
     transparent: true,
-  })
+  });
   let personMat = new THREE.MeshBasicMaterial({
     map: loader.load("media/" + ind + "/main.gif"),
     side: THREE.DoubleSide,
     transparent: true,
-  })
-  for (var i = 0; i < 4; i++) {
+  });
+  for (let i = 0; i < 4; i++) {
     let windowPlane = new THREE.Mesh(plGeo, windowMat);
     let personPlane = new THREE.Mesh(plGeo, personMat);
-    let rad = 40;
+    let rad = 10;
     let posX = rad * Math.sin(i*Math.PI/2);
     let posZ = rad * Math.cos(i*Math.PI/2);
-    personPlane.position.set(posX*1.5, size/4, posZ*1.5);
+    personPlane.position.set(posX*6, size/4, posZ*6);
     personPlane.rotation.y = Math.PI/2 * Math.sin(i*Math.PI/2);
     scenes[ind].add(personPlane);
-    windowPlane.position.set(posX, size*.3, posZ);
+    windowPlane.position.set(posX*8, size*.3, posZ*8);
     windowPlane.rotation.y = Math.PI/2 * Math.sin(i*Math.PI/2);
     scenes[ind].add(windowPlane);
   }
@@ -180,4 +183,31 @@ function createSphere(ind, col){
   let sphere = new THREE.Mesh(spGeo, spMat);
   sphere.position.set(0, 0, -20);
   scenes[ind].add(sphere);
+}
+// scenes
+function scene1(){
+  let phraseSize = 15;
+  let phraseNum  = 12;
+  // create geometry
+  let phGeo = new THREE.PlaneGeometry(phraseSize, phraseSize, 1, 1);
+  for (let i = 0; i < phraseNum; i++) {
+    // load phrase as texture
+    let phMat = new THREE.MeshBasicMaterial({
+      // map: loader.load("media/1/gifs/" + i + ".gif"),
+      map: loader.load("media/1/images/" + i + ".png"),
+      side: THREE.DoubleSide,
+      transparent: true,
+    });
+    // position
+    let rad   = 30 +Math.random()*10;
+    let angle = i*(2*Math.PI/phraseNum) + Math.random()*0.2;
+    let posY = Math.random()*10;
+    let posX = rad * Math.sin(angle);
+    let posZ = rad * Math.cos(angle);
+    // create and add
+    let phrase = new THREE.Mesh(phGeo, phMat);
+    phrase.position.set(posX, posY, posZ);
+    phrase.rotation.y = angle +Math.PI;
+    scenes[1].add(phrase);
+  }
 }
