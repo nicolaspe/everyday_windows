92ee7959652e083c3aca226cb15eb0b62dc82fb8
Nicolas Peña Escarpentier <npescarpentier@gmail.com>
2017-11-24 19:38:49 -0500

    skeleton in scene 1

--- window.js
+++ window.js
@@ -32,9 +32,9 @@ function onLoad(){
 	scenes[3]  = new THREE.Scene();
 	scenes[4]  = new THREE.Scene();
 	scenes[5]  = new THREE.Scene();
-	curr_scene = scenes[0];
+	curr_scene = scenes[1];
 
-  camera = new THREE.PerspectiveCamera(40, wid/hei, 0.1, 1000);
+  camera = new THREE.PerspectiveCamera(50, wid/hei, 0.1, 1000);
 	controls = new THREE.VRControls( camera );
   controls.standing = true;
   camera.position.y = controls.userHeight;
@@ -113,7 +113,11 @@ function createEnvironment(){
 }
 function createScene(ind, col){
   scenes[ind].background = new THREE.Color( 0xeeeeee );
+  let loc = "media/" + ind + "main.gif";
+
   createLights(ind);
+  createFloor(ind);
+  createRoom(ind);
   createSphere(ind, col);
 }
 function createLights(ind){
@@ -124,11 +128,50 @@ function createLights(ind){
   let str = 0.5;
   let d_light = new THREE.DirectionalLight(0xffffff, str);
   scenes[ind].add( d_light );
-
 }
-// create skyDome
+function createFloor(ind){
+  let size = 40;
+  let floorGeo = new THREE.BoxGeometry(size*2, 1, size*2, 10, 2, 10);
+  let floorMat = new THREE.MeshPhongMaterial({
+    color: 0xd0d0d0,
+    specular: 0x000000,
+    flatShading: true,
+    wireframe: true,
+  });
+  let planeF = new THREE.Mesh(floorGeo, floorMat);
+  planeF.position.set(0, -size/5, 0);
+  scenes[ind].add(planeF);
+}
+function createRoom(ind){
+  let size = 40;
+  let plGeo = new THREE.PlaneGeometry(size, size, 10, 10);
+  // images
+  let windowMat = new THREE.MeshBasicMaterial({
+    map: loader.load("media/" + ind + "/window.png"),
+    side: THREE.DoubleSide,
+    transparent: true,
+  })
+  let personMat = new THREE.MeshBasicMaterial({
+    map: loader.load("media/" + ind + "/main.gif"),
+    side: THREE.DoubleSide,
+    transparent: true,
+  })
+  for (var i = 0; i < 4; i++) {
+    let windowPlane = new THREE.Mesh(plGeo, windowMat);
+    let personPlane = new THREE.Mesh(plGeo, personMat);
+    let rad = 40;
+    let posX = rad * Math.sin(i*Math.PI/2);
+    let posZ = rad * Math.cos(i*Math.PI/2);
+    personPlane.position.set(posX*1.5, size/4, posZ*1.5);
+    personPlane.rotation.y = Math.PI/2 * Math.sin(i*Math.PI/2);
+    scenes[ind].add(personPlane);
+    windowPlane.position.set(posX, size*.3, posZ);
+    windowPlane.rotation.y = Math.PI/2 * Math.sin(i*Math.PI/2);
+    scenes[ind].add(windowPlane);
+  }
+}
 function createSphere(ind, col){
-  let spGeo = new THREE.SphereGeometry(2, 25, 25);
+  let spGeo = new THREE.SphereGeometry(1, 25, 25);
 	let spMat = new THREE.MeshPhongMaterial({
 		color: col,
 		// emissive: 0xffffff,
