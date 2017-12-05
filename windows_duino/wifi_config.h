6f867bc09d24bac260006ca8eed5b29e5d3d8e02
Nicolas Peña Escarpentier <npescarpentier@gmail.com>
2017-12-05 08:23:29 -0500

    fixed gif images

--- windows_duino/wifi_config.h
+++ windows_duino/wifi_config.h
@@ -1,5 +1,8 @@
-#define _SSID "npe_WiFi"
-#define _PASS "npescarpentier.1990"
+#define _SSID "Ilana's iPhone"
+#define _PASS "ponypony"
+
+//#define _SSID "npe_WiFi"
+//#define _PASS "npescarpentier.1990"
 
 //#define _SSID "itpsandbox"
 //#define _PASS "NYU+s0a!P?"
